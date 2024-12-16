f72c46311b7bb2f86b0b7c9d445024b5d1369519
Piyush Jaiswal <thejaiswal1@gmail.com>
2024-12-16 13:31:36 +0530

    Added tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',  // Include JS, TS, JSX, and TSX files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
